587905d36bd0ff6582e3dc7799f4b4dbee3a6920
Michael <bejerdk@gmail.com>
2011-11-02 16:38:42 +0100

    nxt_sense sysfs init setup.

--- kernel_development/nxt_sense/nxt_sense.c
+++ kernel_development/nxt_sense/nxt_sense.c
@@ -15,39 +15,74 @@
 
 #define NUMBER_OF_DEVICES 1
 
+#define NUMBER_OF_PORTS 4
+
+#define NONE_CODE 0
+#define TOUCH_CODE 1
+#define LIGHT_CODE 2
+#define MAX_SENSOR_CODE LIGHT_CODE
+
 struct nxt_sense_dev {
   dev_t devt;
   struct cdev cdev;
   struct class *class;
   //	char user_buff[USER_BUFF_SIZE];
   struct device *device;
+  int port_cfg[NUMBER_OF_PORTS];
 };
 
 static struct nxt_sense_dev nxt_sense_dev;
 
+static int load_nxt_sensor(int sensor_code) {
+  return 0;
+}
+
+static int unload_nxt_sensor(int sensor_code) {
+  return 0;
+}
+
+static int update_port_cfg(int cfg[]) {
+  int i;
+  for (i = 0; i < NUMBER_OF_PORTS; ++i) {
+    if (cfg[i] < NONE_CODE || cfg[i] > MAX_SENSOR_CODE) {
+      return -1;
+    }
+  }
+
+  for (i = 0; i < NUMBER_OF_PORTS; ++i) {
+    if (nxt_sense_dev.port_cfg[i] != cfg[i]) {
+      unload_nxt_sensor(nxt_sense_dev.port_cfg[i]);
+      load_nxt_sensor(cfg[i]);
+      nxt_sense_dev.port_cfg[i] = cfg[i];
+    }
+  }
+
+  return 0;
+}
+
 static ssize_t nxt_sense_show(struct device *dev, struct device_attribute *attr, char *buf) {
   
-  return scnprintf(buf, PAGE_SIZE, "Hello world\n");
+  return scnprintf(buf, PAGE_SIZE, "%d %d %d %d\n", nxt_sense_dev.port_cfg[0], nxt_sense_dev.port_cfg[1], nxt_sense_dev.port_cfg[2], nxt_sense_dev.port_cfg[3]);
 }
 
 static ssize_t nxt_sense_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t count) {
-  printk("nxt_sense_store: %s\n", buf);
-
-  int p1, p2, p3, p4;
+  int p[NUMBER_OF_PORTS];
   int res;
 
-  res = sscanf(buf, "%d %d %d %d", &p1, &p2, &p3, &p4);
+  res = sscanf(buf, "%d %d %d %d", &p[0], &p[1], &p[2], &p[3]);
 
-  if (res != 4) {
+  if (res != NUMBER_OF_PORTS) {
     printk("nxt_sense: sysfs input was not four integers!\n");
   } else {
-    printk("nxt_sense: sysfs input: %d %d %d %d\n", p1, p2, p3, p4);
+    printk("nxt_sense: sysfs input: %d %d %d %d\n", p[0], p[1], p[2], p[3]);
+
+    update_port_cfg(p);
   }
 
   return count;
 }
 
-device_ATTR(nxt_sense, 0600, nxt_sense_show, nxt_sense_store);
+DEVICE_ATTR(nxt_sense, 0600, nxt_sense_show, nxt_sense_store);
 
 static const struct file_operations nxt_sense_fops = {
   .owner =	THIS_MODULE,
