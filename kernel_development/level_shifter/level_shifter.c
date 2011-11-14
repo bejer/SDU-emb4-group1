cd243926219e16cdce8b1fdb8b9684dffc6b8253
Michael <bejerdk@gmail.com>
2011-11-14 14:51:03 +0100

    Making ADC use the shared level shifter.

--- kernel_development/level_shifter/level_shifter.c
+++ kernel_development/level_shifter/level_shifter.c
@@ -1,3 +1,6 @@
+/* TODO:
+ * Make sure the "bad" scenarios are handled correctly (such as what to do in initialise_level_shifter if gpio_request fails - should it uninitialise itself and free the level_shifter, and what about error codes?)
+ */
 #include <linux/module.h>
 #include <mach/gpio.h>
 #include <linux/kref.h>
@@ -6,11 +9,9 @@
 
 #define DEVICE_NAME "level_shifter"
 
-/* Only 2OE that is shared? */
-//#define GPIO_1OE 10
+/* Only one level shifter (2OE on it) that is shared for now */
 #define GPIO_2OE 71
 
-/* Should this be encapsulated behind a static defined mutex? to avoid kzallocing twice... */
 DEFINE_MUTEX(ls_mutex);
 
 struct level_shifter {
@@ -19,15 +20,8 @@ struct level_shifter {
 
 static struct level_shifter *level_shifter;
 
-/* What to do when gpio_request fails? call uninitialise_level_shifter(NULL) ? */
+/* What to do when gpio_request fails? (see TODO at the top) */
 static void initialise_level_shifter(void) {
-#if 1
-
-  printk(DEVICE_NAME ": inside initialise_level_shifter\n");
-
-
-#endif
-#if 0
   if (gpio_request(GPIO_2OE, "LS2OE")) {
     printk(KERN_ALERT DEVICE_NAME ": gpio_request failed for LS2OE\n");
   } else {
@@ -35,25 +29,18 @@ static void initialise_level_shifter(void) {
       printk(KERN_ALERT DEVICE_NAME ": gpio_direction_output failed for LS2OE\n");
     }
   }
-#endif
 
   kref_init(&level_shifter->refcount);
 }
 
 static void uninitialise_level_shifter(struct kref *kref) {
-#if 1
-
-  printk(DEVICE_NAME ": inside uninitialise_level_shifter\n");
-
-#endif
-
-#if 0
   gpio_free(GPIO_2OE);
-#endif
+
   kfree(level_shifter);
   level_shifter = NULL;
 }
 
+/* Returns 0 on success, else a negative error code (not implemented yet) */
 static int register_use_of_level_shifter(void) {
   mutex_lock(&ls_mutex);
 
@@ -65,13 +52,7 @@ static int register_use_of_level_shifter(void) {
     }
     initialise_level_shifter();
   } else {
-#if 1
-    printk(DEVICE_NAME ": refcount before register: %d\n", level_shifter->refcount.refcount.counter);
-#endif
     kref_get(&level_shifter->refcount);
-#if 1
-    printk(DEVICE_NAME ": refcount after register: %d\n", level_shifter->refcount.refcount.counter);
-#endif
   }
 
   mutex_unlock(&ls_mutex);
@@ -82,20 +63,10 @@ static int register_use_of_level_shifter(void) {
 static int unregister_use_of_level_shifter(void) {
   mutex_lock(&ls_mutex);
   if (!level_shifter) {
-    printk(KERN_ALERT DEVICE_NAME ": trying to unregister without a register (or someone has unregistered twice), anyhow level_shifter is NULL\n");
+    printk(KERN_ALERT DEVICE_NAME ": trying to unregister without a register (or someone has unregistered twice), anyhow level_shifter is NULL!\n");
     return -1;
   }
-#if 1
-    printk(DEVICE_NAME ": refcount before unregister: %d\n", level_shifter->refcount.refcount.counter);
-#endif
   kref_put(&level_shifter->refcount, uninitialise_level_shifter);
-#if 1
-  if (!level_shifter) {
-    printk(DEVICE_NAME ": refcount after unregister: level_shifter is null - fully dereferenced\n");
-  } else {
-    printk(DEVICE_NAME ": refcount after unregister: %d\n", level_shifter->refcount.refcount.counter);
-  }
-#endif
 
   mutex_unlock(&ls_mutex);
 
